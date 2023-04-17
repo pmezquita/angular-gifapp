b1768631258fce0392f75f0e5cb79b98fec96487
pmezquita <ing.pedromezquita@gmail.com>
2023-04-17 11:04:47 -0600

    [ADD] Consumir api de gifs con http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {GifsModule} from "./gifs/gifs.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -11,10 +12,12 @@ import {GifsModule} from "./gifs/gifs.module";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +7,14 @@ import {Injectable} from '@angular/core';
 export class GifsService {
 
   private _historial: string[] = [];
+  private apiKey: string = 'qC7cXKpPcSnVnBpfpzfv54jdSXWTqIOr';
 
 
   get historial(): string[] {
     return [...this._historial];
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   buscarGifs(query: string = '') {
@@ -24,6 +26,10 @@ export class GifsService {
       this._historial = this.historial.splice(0, 10);
     }
 
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=qC7cXKpPcSnVnBpfpzfv54jdSXWTqIOr&limit=10&q=dbz')
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
+
   }
 }
