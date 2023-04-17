75be52e33f23c840142ca74543abe9eb708501fc
pmezquita <ing.pedromezquita@gmail.com>
2023-04-17 10:07:26 -0600

    [ADD] Obtener valor de input con viewchild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class BusquedaComponent {
 
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+  }
 }
